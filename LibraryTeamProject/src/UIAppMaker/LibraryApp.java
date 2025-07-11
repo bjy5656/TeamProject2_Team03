52517de3e5edd64894758a36adeab26c2e267dd2
ericlee1103 <jhoon1103@gmail.com>
2025-07-11 14:21:00 +0900

    250711 14:20 LibraryApp 추가

--- LibraryTeamProject/src/UIAppMaker/LibraryApp.java
+++ LibraryTeamProject/src/UIAppMaker/LibraryApp.java
@@ -1,7 +1,19 @@
 package UIAppMaker;
 
+import java.util.Scanner;
+
+import member.Member;
+import serviceDeveloper.LibraryService;
+
 public class LibraryApp {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Member member = new Member();
+		ConsoleUI UI = new ConsoleUI(member);
+		LibraryService library = new LibraryService();
 		
+		UI.loginMenu(sc);
+		
+		sc.close();
 	}
 }
