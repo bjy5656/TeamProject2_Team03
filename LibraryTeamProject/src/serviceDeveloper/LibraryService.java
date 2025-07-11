25b8134c39621096152e5c5c5cb05b0ae4d0b7f0
LeeTaeHo00 <th3179@naver.com>
2025-07-11 12:50:20 +0900

    250711 12:49 책 반납 대출 메소드, 책 추가 메소드 작업

--- LibraryTeamProject/src/serviceDeveloper/LibraryService.java
+++ LibraryTeamProject/src/serviceDeveloper/LibraryService.java
@@ -19,14 +19,21 @@ public class LibraryService {
 		randomBookList();
 	}
 
+	// 현재 책목록을 모두 출력하는 메소드
 	void printBookList() 
 	{		
 		ConsoleUI.printBookList(bookList);
 	}
 	
+	// 유저가 책을 빌리는 메소드
+	// 만약 유저가 빌린 책 목록에 해당 인덱스의 책을 add 해준다.
+	// 해당 책과 같은 이름의 도서관 책 목록에 있는 책의 대출 가능 상태를 false로 바꿔줌
+		
+	// borrowBook 에서는 해당 
+	
 	void borrowBookService(Member user, String bookName) 
 	{
-		int index = findBookIndex(bookList, bookName);
+		int index = findBookIndex(bookList, bookName, true);
 		boolean isFound = index != -1 ? true : false;
 		
 		if(isFound) 
@@ -53,20 +60,23 @@ public class LibraryService {
 		}
 	}
 	
+	// 랜덤으로 책을 5개 생성 해주는 메소드
 	private void randomBookList() 
 	{
 		for(int i = 0; i < 5; i++) 
 		{
 			int randomName = (int)(Math.random() * nameList.length);
 			int randomAuthor = (int)(Math.random() * author.length);
+			int randomPublisher = (int)(Math.random() * publisher.length);
 			
-			bookList.add(new Book(nameList[randomName], author[randomAuthor]));
+			addBook(nameList[randomName], author[randomAuthor], publisher[randomPublisher]);
 		}
 	}
 	
-	public void addBook(String name, String author) 
+	// 책을 추가해주는 메소드
+	public void addBook(String name, String author, String publisher) 
 	{
-		Book newBook = new Book(name, author);
+		Book newBook = new Book(name, author, publisher);
 		bookList.add(newBook);
 	}
 	
@@ -86,7 +96,7 @@ public class LibraryService {
 		return -1;
 	}
 	
-	// 대출 받은 책을 출력할지, 혹은 대출 안된 책을 검사할지 정해주기 위해 오버로딩
+	// 대출이 가능한 책 혹은 대출이 불가능한 책을 정해서 인덱스를 반환 하도록 오버로딩
 	int findBookIndex(ArrayList<Book> targetList, String targetName, boolean checkReturn) 
 	{
 		int index = 0;;
