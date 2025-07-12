a9411569733b189942b2ac53ba0c98a9366cd813
LeeTaeHo00 <th3179@naver.com>
2025-07-12 18:39:22 +0900

    259712 18:40 getter 추가, toString메소드 오버라이딩

--- LibraryTeamProject/src/serviceDeveloper/LibraryService.java
+++ LibraryTeamProject/src/serviceDeveloper/LibraryService.java
@@ -10,11 +10,11 @@ import exceptionMaker.MaxBorrowException;
 import member.Member;
 
 public class LibraryService {	
-	ArrayList<Book> bookList = new ArrayList<>();
+	private ArrayList<Book> bookList = new ArrayList<>();
 	
-	String[] nameList = {"자바", "데이터베이스", "보안", "부자아빠", "리눅스마스터", "SQLD"};
-	String[] author = {"이정훈", "민경승", "백정이", "이태호", "이재빈", "김태현"};
-	String[] publisher = {"잉", "엥", "옹"};
+	private String[] nameList = {"자바", "데이터베이스", "보안", "부자아빠", "리눅스마스터", "SQLD"};
+	private String[] author = {"이정훈", "민경승", "백정이", "이태호", "이재빈", "김태현"};
+	private String[] publisher = {"잉", "엥", "옹"};
 	
 
 	public LibraryService() {
@@ -26,6 +26,11 @@ public class LibraryService {
 	public void printBookList() 
 	{		
 		ConsoleUI.printBookList(bookList);
+	}
+	
+	// getter
+	public ArrayList<Book> getBookList() {
+		return bookList;
 	}
 	
 	// 유저가 책을 빌리는 메소드
@@ -45,12 +50,11 @@ public class LibraryService {
 			bookList.get(index).borrowBook();
 		}
 	}
-	
+
 	// 유저가 빌린 책을 반납하는 메소드
 	// 만약 유저가 빌린 책 목록에 같은 이름을 가진 책이 있다면
 	// 해당 책을 리스트에서 remove 해주고
 	// 해당 책과 같은 이름의 도서관 책 목록에 있는 책의 대출 가능 상태를 true로 바꿔줌
-	
 	// returnbook 에서는 책에 index로 받아서 index에 있는 값을 제거
 	public void returnBookService(Member user, String bookName) 
 	{		
@@ -112,5 +116,11 @@ public class LibraryService {
 		
 		return -1;
 	}
+	
+	// toString 메소드 오버라이딩
+		@Override
+		public String toString() {
+			return "도서관 관리 메소드 입니다.";
+		}
 	
 }
