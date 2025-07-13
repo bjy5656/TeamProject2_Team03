8960997647e080f454bb54e88a940c33f303f05c
ericlee1103 <jhoon1103@gmail.com>
2025-07-13 10:25:37 +0900

    250713 10:25 LibraryApp 추가

--- LibraryTeamProject/src/UIAppMaker/LibraryApp.java
+++ LibraryTeamProject/src/UIAppMaker/LibraryApp.java
@@ -6,16 +6,19 @@ import member.Member;
 import serviceDeveloper.LibraryService;
 
 public class LibraryApp {
+	public static LibraryService library = new LibraryService();
+	public static ConsoleUI UI = new ConsoleUI(null);
+	public static LoginManager server = new LoginManager();
+	
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		Member member = new Member();
-		ConsoleUI UI = new ConsoleUI(member);
-		LibraryService library = new LibraryService();
+		Member member = null;
 		
+		UI.setCurLoginUser(member);
 		UI.loginMenu(sc);
 		
 		sc.close();
 
 	}
-}
+}	
