ae2c9fe371c6033f20a0d8b483633c5c3bd0a7ab
bjy5656 <qorwjddl1234@gmail.com>
2025-07-22 09:11:52 +0900

    250722 09:11 Book, BookDAO 추가

--- LibraryTeamProject/src/bookDTO/Book.java
+++ LibraryTeamProject/src/bookDTO/Book.java
@@ -6,92 +6,80 @@ import exceptionMaker.BookAlreadyException;
 import exceptionMaker.BookNotAvailableException;
 import exceptionMaker.MaxBorrowException;
 
-public class Book implements Borrow {
-	// 필드
-	private String title;
-	private String author;
-	private String publisher;
-	private boolean isAvailable = true;
-	
-	// 생성자
-	public Book(String title, String author, String publisher) {
-		this.title = title;
-		this.author = author;
-		this.publisher = publisher;
-	}
-	
-	
-	// 메소드 오버라이딩
-	// 도서대출 오버라이딩 -> 메시지 출력, 도서 정보 출력, 대출가능 여부 변경(false), 예외 던지기
-	@Override
-	public void borrowBook() throws BookNotAvailableException{
-		if(!isAvailable) {
-			throw new BookNotAvailableException();
-		}
-		System.out.println("도서를 대출합니다!");
-//		printInfo();
-		System.out.println(this);
-		isAvailable = false;
-	}
-
-	// 도서반납 오버라이딩 -> 메시지 출력, 도서 정보 출력, 대출가능 여부 변경(true)
-	@Override
-	public void returnBook() {
-		System.out.println("도서를 반납합니다!");
-//		printInfo();
-		System.out.println(this);
-		isAvailable = true;
-	}
-
-	// hashCode 오버라이딩 -> 이름, 저자, 출판사를 이용해서 생성한 해시값 반환
-	@Override
-	public int hashCode() {
-		return Objects.hash(title, author, publisher);
-	}
-
-	// equals 오버라이딩 -> 제목, 저자, 출판사가 같으면 같은 객체로 판단
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if(obj instanceof Book) {
-			Book book = (Book)obj;
-	        return this.title.equals(book.title) && this.author.equals(book.author) && this.publisher.equals(book.publisher);
-		}
-		return false;
-	}
-
-	// toString 오버라이딩 -> 도서 정보 반환
-	@Override
-	public String toString() {
-		return "[제목 : " + title + ", 저자 : " + author + ", 출판사 : " + publisher + ", 대출가능 여부 : "
-				+ (isAvailable ? "대출가능" : "대출불가") + "]";
-	}
-	
-//	//메소드
-//	//도서정보 출력 메소드
-//	public void printInfo() {
-//		System.out.println("책 이름 : " + getTitle());
-//		System.out.println("저자 : " + getAuthor());
-//	}
-	
-	// getter, setter
-	public void setAvailable(boolean isAvailable) {
-		this.isAvailable = isAvailable;
-	}
-	public boolean isAvailable() {
-		return isAvailable;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public String getAuthor() {
-		return author;
-	}
-	public String getPublisher() {
-		return publisher;
-	}
+public class Book{
+   //sql
+//   CREATE TABLE TBL_BOOKS(
+//         BOOK_NUMBER NUMBER,
+//         BOOK_TITLE varchar2(100) NOT NULL,
+//         BOOK_AUTHOR varchar2(100) NOT NULL,
+//         BOOK_AVAILABLE Char(1) DEFAULT 'T' NOT NULL,
+//         USER_NUMBER NUMBER,
+//         CONSTRAINT PK_BOOK_NUMBER PRIMARY KEY(book_number),
+//         CONSTRAINT FK_USER_NUMBER FOREIGN KEY(USER_NUMBER) REFERENCES TBL_USER (USER_NUMBER),
+//         CHECK(BOOK_AVAILABLE IN ('T', 'F'))
+//      );
+   
+   // 필드
+   private int bookNumber;
+   private String bookTitle;
+   private String bookAuthor;
+   private boolean bookAvailable;
+   private int userNumber;
+   
+   // 생성자 - 회원번호 없이 생성
+   public Book(String bookTitle, String bookAuthor, boolean bookAvailable) {
+      this.bookTitle = bookTitle;
+      this.bookAuthor = bookAuthor;
+      this.bookAvailable = bookAvailable;
+   }
+   // 생성자 - 회원번호 있이 생성
+   public Book(String bookTitle, String bookAuthor, boolean bookAvailable, int userNumber) {
+	      this.bookTitle = bookTitle;
+	      this.bookAuthor = bookAuthor;
+	      this.bookAvailable = bookAvailable;
+	      this.userNumber = userNumber;
+	   }
+   
+   //getter, setter
+   public int getBookNumber() {
+      return bookNumber;
+   }
+   public void setBookNumber(int bookNumber) {
+      this.bookNumber = bookNumber;
+   }
+   public String getBookTitle() {
+      return bookTitle;
+   }
+   public void setBookTitle(String bookTitle) {
+      this.bookTitle = bookTitle;
+   }
+   public String getBookAuthor() {
+      return bookAuthor;
+   }
+   public void setBookAuthor(String bookAuthor) {
+      this.bookAuthor = bookAuthor;
+   }
+   public boolean isBookAvailable() {
+      return bookAvailable;
+   }
+   public void setBookAvailable(boolean bookAvailable) {
+      this.bookAvailable = bookAvailable;
+   }
+   public int getUserNumber() {
+      return userNumber;
+   }
+   public void setUserNumber(int userNumber) {
+      this.userNumber = userNumber;
+   }
+   
+   // toString 오버라이딩 -> 도서 정보 반환
+   @Override
+   public String toString() {
+      return "[도서번호 : " + bookNumber
+            + ", 제목 : " + bookTitle 
+            + ", 저자 : " + bookAuthor 
+            + ", 대출가능 여부 : " + (bookAvailable ? "가능" : "불가") 
+            + ", 회원번호 : " + userNumber + "]";
+   }
 
 }
